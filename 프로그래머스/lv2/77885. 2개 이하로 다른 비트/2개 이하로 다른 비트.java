9ee4e57fe5c6a998a3118974a3da518350922d3a
ShinKwang2 <71647602+ShinKwang2@users.noreply.github.com>
2023-09-11 16:50:19 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 122.94 ms, Memory: 147 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+class Solution {
+    public long[] solution(long[] numbers) {
+        
+        long[] answer = new long[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            
+            if (numbers[i] % 2 == 0) {
+                answer[i] = numbers[i] + 1;
+                continue;
+            }
+            
+            String binary = toBinary(numbers[i]);
+            
+            if (!binary.contains("0")) {
+                binary = "0" + binary;
+                binary = String.valueOf(binary.charAt(1)) + binary.charAt(0) + binary.substring(2);
+            }
+            else {
+                int lastIndex = binary.lastIndexOf("0");
+                if (lastIndex == binary.length() - 1) {
+                    binary = binary.substring(0, lastIndex) + "1";
+                }
+                else {
+                    binary = binary.substring(0, lastIndex) + "10" + binary.substring(lastIndex + 2); 
+                }
+            }
+            answer[i] = binaryToLong(binary);
+        }
+                
+        return answer;
+    }
+    
+    private String toBinary(long number) {
+        
+        StringBuilder sb = new StringBuilder();
+        while(number != 0) {
+            sb.append(number % 2);
+            number /= 2;
+        }
+        return sb.reverse().toString();
+        
+    }
+    
+    private long binaryToLong(String binary) {
+        String target = new StringBuilder(binary).reverse().toString();
+        long answer = 0;
+        long current = 1L;
+        for (int i = 0; i < target.length(); i++) {
+            if (target.charAt(i) == '1') {
+                answer += current;
+            }
+            current *= 2;
+        }
+        
+        return answer;
+    }
+}
