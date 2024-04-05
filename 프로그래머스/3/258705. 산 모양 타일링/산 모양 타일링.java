cb6b60b9072604ee7578a9e6fe118902acd58b21
ShinKwang2 <71647602+ShinKwang2@users.noreply.github.com>
2024-04-05 13:32:50 +0900

    [level 3] Title: 산 모양 타일링, Time: 12.27 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -0,0 +1,58 @@
+class Solution {
+    public int solution(int n, int[] tops) {
+        
+        final int MOD = 10007;
+        
+        int[] makeAll = new int[n];
+        int[] makeMinus = new int[n];
+        
+        if (tops[0] == 1) {
+            makeAll[0] = 3;
+            makeMinus[0] = 1;
+        }
+        else {
+            makeAll[0] = 2;
+            makeMinus[0] = 1;
+        }
+        
+        for (int i = 1; i < n; i++) {
+            if (tops[i] == 1) {
+                int all = makeAll[i - 1] * 3 % MOD;
+                int minus = makeAll[i - 1] % MOD;
+                // makeAll[i] += (makeAll[i - 1] * 3); // 3개 만들 수 있고
+                // makeMinus[i] += makeAll[i - 1]; // 1개
+                makeAll[i] += all;
+                makeMinus[i] += minus;
+                
+                all = makeMinus[i - 1] * 2 % MOD;
+                minus = makeMinus[i - 1] % MOD;
+                // makeAll[i] += (makeMinus[i - 1] * 2); // 2개 만들 수 있고
+                // makeMinus[i] += makeMinus[i - 1]; // 1개 
+                makeAll[i] += all;
+                makeMinus[i] += minus;
+            }
+            else {
+                int all = makeAll[i - 1] * 2 % MOD;
+                int minus = makeAll[i - 1] % MOD;
+                // makeAll[i] += (makeAll[i - 1] * 2); // 2개 만들 수 있고
+                // makeMinus[i] += makeAll[i - 1]; // 1개
+                makeAll[i] += all;
+                makeMinus[i] += minus;
+                
+                all = makeMinus[i - 1] % MOD;
+                minus = makeMinus[i - 1] % MOD;
+                // makeAll[i] += (makeMinus[i - 1]); // 1개 만들 수 있고
+                // makeMinus[i] += makeMinus[i - 1]; // 1개 
+                makeAll[i] += all;
+                makeMinus[i] += minus;
+            }
+            makeAll[i] %= MOD;
+            makeMinus[i] %= MOD;
+        }
+        // System.out.println(makeAll[n - 1] + makeMinus[n - 1]);
+        
+        
+        int answer = (makeAll[n - 1] + makeMinus[n - 1]) % MOD;
+        return answer;
+    }
+}
