fd4bacd465b1abd07dfd301fd5e428b6fa881650
ShinKwang2 <71647602+ShinKwang2@users.noreply.github.com>
2024-04-06 10:37:11 +0900

    [level 3] Title: 산 모양 타일링, Time: 6.42 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -3,8 +3,8 @@ class Solution {
         
         final int MOD = 10007;
         
-        int[] makeAll = new int[n];
-        int[] makeMinus = new int[n];
+        int[] makeAll = new int[n]; // 다음 삼각형에 영향을 안주는 완전 삼감형
+        int[] makeMinus = new int[n]; // 다음 삼각형에 영향을 주는 불완전 삼각형
         
         if (tops[0] == 1) {
             makeAll[0] = 3;
@@ -17,8 +17,8 @@ class Solution {
         
         for (int i = 1; i < n; i++) {
             if (tops[i] == 1) {
-                int all = makeAll[i - 1] * 3 % MOD;
-                int minus = makeAll[i - 1] % MOD;
+                int all = makeAll[i - 1] * 3 % MOD; // 3
+                int minus = makeAll[i - 1] % MOD; // 1
                 // makeAll[i] += (makeAll[i - 1] * 3); // 3개 만들 수 있고
                 // makeMinus[i] += makeAll[i - 1]; // 1개
                 makeAll[i] += all;
@@ -32,8 +32,8 @@ class Solution {
                 makeMinus[i] += minus;
             }
             else {
-                int all = makeAll[i - 1] * 2 % MOD;
-                int minus = makeAll[i - 1] % MOD;
+                int all = makeAll[i - 1] * 2 % MOD; // 2
+                int minus = makeAll[i - 1] % MOD;  // 1
                 // makeAll[i] += (makeAll[i - 1] * 2); // 2개 만들 수 있고
                 // makeMinus[i] += makeAll[i - 1]; // 1개
                 makeAll[i] += all;
@@ -46,8 +46,8 @@ class Solution {
                 makeAll[i] += all;
                 makeMinus[i] += minus;
             }
-            makeAll[i] %= MOD;
-            makeMinus[i] %= MOD;
+            // makeAll[i] %= MOD;
+            // makeMinus[i] %= MOD;
         }
         // System.out.println(makeAll[n - 1] + makeMinus[n - 1]);
         
