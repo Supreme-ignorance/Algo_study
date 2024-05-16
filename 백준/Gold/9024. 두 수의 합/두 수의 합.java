0bd3675097a0162cf9e6bb9a60d2f28869ad58ec
ShinKwang2 <71647602+ShinKwang2@users.noreply.github.com>
2024-05-16 19:32:27 +0900

    [Gold V] Title: 두 수의 합, Time: 2152 ms, Memory: 227108 KB -BaekjoonHub

---
+++ 백준/Gold/9024. 두 수의 합/두 수의 합.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int tc = 0; tc < T; tc++) {
+            StringTokenizer tokenizer = new StringTokenizer(br.readLine(), " ");
+            int N = Integer.parseInt(tokenizer.nextToken());
+            int K = Integer.parseInt(tokenizer.nextToken());
+
+            tokenizer = new StringTokenizer(br.readLine(), " ");
+            int[] numbers = new int[N];
+            for (int i = 0; i < N; i++) {
+                numbers[i] = Integer.parseInt(tokenizer.nextToken());
+            }// Setting
+
+            Arrays.sort(numbers);
+
+            int minAbs = Integer.MAX_VALUE;
+            int count = 0;
+
+            int leftIdx = 0;
+            int rightIdx = N - 1;
+            while (leftIdx < rightIdx) {
+                int sum = numbers[leftIdx] + numbers[rightIdx];
+                int absValue = Math.abs(sum - K);
+                if (absValue == minAbs) {
+                    count++;
+                }
+                else if (absValue < minAbs) {
+                    minAbs = absValue;
+                    count = 1;
+                }
+
+                if (sum >= K) {
+                    rightIdx--;
+                }
+                else {
+                    leftIdx++;
+                }
+//                else if (sum > K) {
+//                    rightIdx--;
+//                }
+//                else { // 같다면
+//                    int leftAbsValue = Math.abs(numbers[leftIdx] - numbers[leftIdx + 1]);
+//                    int rightAbsValue = Math.abs(numbers[rightIdx] - numbers[rightIdx - 1]);
+//                    if (leftAbsValue <= rightAbsValue) {
+//                        leftIdx++;
+//                    }
+//                    else {
+//                        rightIdx++;
+//                    }
+//                }
+            }
+            sb.append(count).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
