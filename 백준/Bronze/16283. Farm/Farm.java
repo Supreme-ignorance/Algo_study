05cd8e3be91a7dbeca60faff0d67e77e2895f882
ShinKwang2 <71647602+ShinKwang2@users.noreply.github.com>
2025-03-27 10:59:15 +0900

    [Bronze II] Title: Farm, Time: 92 ms, Memory: 13020 KB -BaekjoonHub

--- 백준/Bronze/16283. Farm/Farm.java
+++ 백준/Bronze/16283. Farm/Farm.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -9,21 +11,27 @@ public class Main {
         int n = kb.nextInt();
         int w = kb.nextInt();
 
-        String answer = "-1";
+        //String answer = "-1";
+        List<int[]> answer = new ArrayList<>();
 
         for (int i = 1; i < n; i++) { // 양이 1마리인것부터 찾자
             int rest = w - (i * a); // 염소에게 남은 양
             if (rest % b == 0 && n == i + (rest / b)) {
-                if (answer.equals("-1")) {
-                    answer = i + " " + (n - i);
+                if (answer.isEmpty()) {
+                    answer.add(new int[]{i, n - i});
                 }
                 else {
-                    answer = "-1";
+                    answer.add(new int[]{i, n - i});
                     break;
                 }
             }
         }
 
-        System.out.println(answer);
+        if (answer.size() != 1) {
+            System.out.println(-1);
+        }
+        else {
+            System.out.println(answer.get(0)[0] + " " + answer.get(0)[1]);
+        }
     }
 }
