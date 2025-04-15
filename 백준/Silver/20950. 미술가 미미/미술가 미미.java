31185e68d732c8863dc6283a732a2e64dfc54656
ShinKwang2 <71647602+ShinKwang2@users.noreply.github.com>
2025-04-15 12:44:11 +0900

    [Silver II] Title: 미술가 미미, Time: 120 ms, Memory: 12024 KB -BaekjoonHub

--- 백준/Silver/20950. 미술가 미미/미술가 미미.java
+++ 백준/Silver/20950. 미술가 미미/미술가 미미.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,6 +11,7 @@ public class Main {
     static int[] target = new int[3];
 
     static int min = Integer.MAX_VALUE;
+//    static int[] selected;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,34 +28,32 @@ public class Main {
         target[1] = Integer.parseInt(t.nextToken());
         target[2] = Integer.parseInt(t.nextToken());
 
-        recur(0, 0, 0, 0, 0);
+        for (int i = 2; i <= 7; i++) {
+//            selected = new int[i];
+            comb(0, 0, 0, 0, 0, i);
+        }
         System.out.println(min);
     }
 
-    static void isMin(int red, int green, int blue) {
+    static int isMin(int red, int green, int blue) {
         int current = Math.abs(red - target[0]) + Math.abs(green - target[1]) + Math.abs(blue - target[2]);
         min = Math.min(min, current);
+        return current;
     }
 
-    static void recur(int cur, int r, int g, int b, int count) {
+    static void comb(int cur, int start, int r, int g, int b, int limit) {
         // 기저 조건
-        if (cur == N) {
-            if (count != 0 && count != 1 && count <= 7) {
-                int red = r / count;
-                int green = g / count;
-                int blue = b / count;
-                isMin(red, green, blue);
-            }
+        if (cur >= limit) {
+//            System.out.println(Arrays.toString(selected));
+            int red = r / limit;
+            int green = g / limit;
+            int blue = b / limit;
+            isMin(red, green, blue);
             return;
         }
 
-        if (count > 7) {
-            return;
+        for (int i = start; i < N; i++) {
+            comb(cur + 1, i + 1, r + RGB[i][0], g + RGB[i][1], b + RGB[i][2], limit);
         }
-
-        // 현재 물감 사용하기
-        recur(cur + 1, r + RGB[cur][0], g + RGB[cur][1], b + RGB[cur][2], count + 1);
-        // 현재 물감 사용 안하기
-        recur(cur + 1, r, g, b, count);
     }
 }
