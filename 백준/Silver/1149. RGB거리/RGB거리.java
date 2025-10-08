0dccec21d0e97b60841deaf5ef71eb2b958173bf
ShinKwang2 <71647602+ShinKwang2@users.noreply.github.com>
2025-10-08 14:04:41 +0900

    [Silver I] Title: RGB거리, Time: 140 ms, Memory: 16924 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -5,7 +5,6 @@ public class Main {
     static int N;
     static int[][] grid;
     static int[][] dp;
-
     static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) {
@@ -28,12 +27,6 @@ public class Main {
         for (int i = 0; i < 3; i++) {
             recur(0, i);
         }
-//        for (int i = 0; i < N; i++) {
-//            for (int j = 0; j < 3; j++) {
-//                System.out.print(dp[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
 
         for (int i = 0; i < 3; i++) {
             min = Math.min(min, dp[0][i]);
@@ -45,13 +38,14 @@ public class Main {
         if (cur == N) {
             return 0;
         }
+        // 이미 이렇게 최소값을 구해놨었다면 굳이 더 볼필요 없으니
         if (dp[cur][prev] != -1) {
             return dp[cur][prev];
         }
 
-        int ret = (int) 1e9; // 절대 선택될 수 없는 값으로 설정
+        int ret = (int) 1e9;
         for (int i = 0; i < 3; i++) {
-            if (prev == i) continue;
+            if (i == prev) continue;
             ret = Math.min(ret, recur(cur + 1, i) + grid[cur][i]);
         }
         dp[cur][prev] = ret;
