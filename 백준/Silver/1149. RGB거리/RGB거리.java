94ab795099526d207a1bbeeb6d34f61b9b953fca
ShinKwang2 <71647602+ShinKwang2@users.noreply.github.com>
2025-10-22 09:26:51 +0900

    [Silver I] Title: RGB거리, Time: 76 ms, Memory: 12228 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,54 +1,62 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
     static int N;
     static int[][] grid;
-    static int[][] dp;
-    static int min = Integer.MAX_VALUE;
 
-    public static void main(String[] args) {
-        Scanner kb = new Scanner(System.in);
-        N = kb.nextInt();
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
         grid = new int[N][3];
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < 3; j++) {
-                grid[i][j] = kb.nextInt();
-            }
+            StringTokenizer tokenizer = new StringTokenizer(br.readLine(), " ");
+            grid[i][0] = Integer.parseInt(tokenizer.nextToken());
+            grid[i][1] = Integer.parseInt(tokenizer.nextToken());
+            grid[i][2] = Integer.parseInt(tokenizer.nextToken());
         }
 
         dp = new int[N][3];
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < 3; j++) {
-                dp[i][j] = -1;
-            }
+            Arrays.fill(dp[i], -1);
         }
 
         for (int i = 0; i < 3; i++) {
             recur(0, i);
         }
-
+        int min = Integer.MAX_VALUE;
         for (int i = 0; i < 3; i++) {
             min = Math.min(min, dp[0][i]);
         }
         System.out.println(min);
+        
     }
 
+    // 현재는 앞서 최소비용으로 칠해왔음
     static int recur(int cur, int prev) {
+        // 마지막은 칠할 것이 없으므로 0에서 시작한다.
         if (cur == N) {
             return 0;
         }
-        // 이미 이렇게 최소값을 구해놨었다면 굳이 더 볼필요 없으니
+
+        // 앞으로 최소비용으로 칠할 것을 이미 계산했다면
         if (dp[cur][prev] != -1) {
             return dp[cur][prev];
         }
 
-        int ret = (int) 1e9;
+        // 앞으로 최소비용으로 칠할 것을 아직 계산하지 않았다면
+        int min = (int) 1e9;
         for (int i = 0; i < 3; i++) {
             if (i == prev) continue;
-            ret = Math.min(ret, recur(cur + 1, i) + grid[cur][i]);
+            min = Math.min(min, recur(cur + 1, i) + grid[cur][i]);
         }
-        dp[cur][prev] = ret;
+        dp[cur][prev] = min;
         return dp[cur][prev];
     }
 }
